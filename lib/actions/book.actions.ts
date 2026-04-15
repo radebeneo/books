e05e0e2e20a2d3b5ccfba201499b54f333e2fc10
radebeneo <radebeneo17@gmail.com>
2026-04-15 15:04:55 +0200

    implemented createBook function as part of the book actions.

--- lib/actions/book.actions.ts
+++ lib/actions/book.actions.ts
@@ -1,11 +1,28 @@
 import {CreateBook} from '@/types'
 import {connectToDatabase} from "@/database/mongoose";
+import {generateSlug, serializeData} from "@/lib/utils";
+import Book from "@/database/models/book.model";
 
 export const createBook = async (data: CreateBook) => {
     try{
         await connectToDatabase()
+        const slug = generateSlug(data.title)
+        const existingBook = await Book.findOne({slug}).lean();
 
-        const slug = generateSlug()
+        if(existingBook){
+            return {
+                success: true,
+                data: serializeData(existingBook),
+                alreadyExists: true
+            }
+        }
+
+        const book = await Book.create({...data, slug, totalSegments: 0})
+
+        return {
+            success: true,
+            data: serialzeData(book)
+        }
     } catch (e) {
         console.error('Error creating book: ', e)
         return{
